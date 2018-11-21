4986d551d0fed1200bff2ed9bce931260bf5c34e
thomasbtatum <thomasbtatum@users.noreply.github.com>
2018-11-21 17:47:09 +0100

    new breakup and comment

--- AdventOfCode2017Day3/Program.cs
+++ AdventOfCode2017Day3/Program.cs
@@ -4,6 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+namespace AdventOfCode2017Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Part1();
+            Part2();
+
+
+        }
+
+
+        static void Part1()
+        {
+
+
 /*
 --- Day 3: Spiral Memory ---
 You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
@@ -27,13 +48,6 @@ How many steps are required to carry the data from the square identified in your
 
 Your puzzle answer was 552.
 */
-
-namespace AdventOfCode2017Day3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
 
             //int targetValue = 325489;
             //int targetValue = 12;
@@ -68,7 +82,7 @@ namespace AdventOfCode2017Day3
                         //else 
                         if (checkEvenOrOdd % 2 == 1)
                         {//odd
-                            run =  rise - mod;
+                            run = rise - mod;
                         }
                         else
                         {//even
@@ -87,12 +101,43 @@ namespace AdventOfCode2017Day3
                 System.Console.WriteLine("Rise: {0} Run: {1} Man Distance: {2}", rise, run, manDist);
                 c = int.Parse(Console.ReadLine());
 
-                if(c == -999)
+                if (c == -999)
                 {
                     break;
                 }
 
             }
         }
+
+        static void Part2()
+        {
+
+/*
+As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. 
+Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+
+So, the first few squares' values are chosen as follows:
+
+Square 1 starts with the value 1.
+Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+
+147  142  133  122   59
+304    5    4    2   57
+330   10    1    1   54
+351   11   23   25   26
+362  747  806--->   ...
+What is the first value written that is larger than your puzzle input?
+
+Your puzzle input is still 325489.
+*/
+
+
+        }
+
+
     }
 }
