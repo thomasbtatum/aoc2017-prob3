5e022174c53c459a80ef49640ff7d79bb8d37480
thomasbtatum <thomasbtatum@users.noreply.github.com>
2018-11-23 17:44:33 +0100

    incremental check in

--- AdventOfCode2017Day3/Program.cs
+++ AdventOfCode2017Day3/Program.cs
@@ -14,7 +14,7 @@ namespace AdventOfCode2017Day3
         {
 
 
-            Part1();
+            //Part1();
             Part2();
 
 
@@ -25,29 +25,29 @@ namespace AdventOfCode2017Day3
         {
 
 
-/*
---- Day 3: Spiral Memory ---
-You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
+            /*
+            --- Day 3: Spiral Memory ---
+            You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
 
-Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
+            Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
 
-17  16  15  14  13
-18   5   4   3  12
-19   6   1   2  11
-20   7   8   9  10
-21  22  23---> ...
-While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
+            17  16  15  14  13
+            18   5   4   3  12
+            19   6   1   2  11
+            20   7   8   9  10
+            21  22  23---> ...
+            While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
 
-For example:
+            For example:
 
-Data from square 1 is carried 0 steps, since it's at the access port.
-Data from square 12 is carried 3 steps, such as: down, left, left.
-Data from square 23 is carried only 2 steps: up twice.
-Data from square 1024 must be carried 31 steps.
-How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
+            Data from square 1 is carried 0 steps, since it's at the access port.
+            Data from square 12 is carried 3 steps, such as: down, left, left.
+            Data from square 23 is carried only 2 steps: up twice.
+            Data from square 1024 must be carried 31 steps.
+            How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
 
-Your puzzle answer was 552.
-*/
+            Your puzzle answer was 552.
+            */
 
             //int targetValue = 325489;
             //int targetValue = 12;
@@ -109,31 +109,107 @@ Your puzzle answer was 552.
             }
         }
 
-        static void Part2()
+        
+        public enum Wall
         {
 
-/*
-As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. 
-Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+            EastWall  = 0,
+            NorthWall = 1,
+            WestWall  = 2,
+            SouthWall = 3
+        }
 
-So, the first few squares' values are chosen as follows:
+        static void Part2()
+        {
+            var orderedWalls = Enum.GetValues(typeof(Wall));
 
-Square 1 starts with the value 1.
-Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
-Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
-Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
-Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
-Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
 
-147  142  133  122   59
-304    5    4    2   57
-330   10    1    1   54
-351   11   23   25   26
-362  747  806--->   ...
-What is the first value written that is larger than your puzzle input?
+            /*
+            As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. 
+            Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
 
-Your puzzle input is still 325489.
-*/
+            So, the first few squares' values are chosen as follows:
+
+            Square 1 starts with the value 1.
+            Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+            Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+            Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+            Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+            Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+
+            147  142  133  122   59
+            304    5    4    2   57
+            330   10    1    1   54
+            351   11   23   25   26
+            362  747  806--->   ...
+            What is the first value written that is larger than your puzzle input?
+
+            Your puzzle input is still 325489.
+            */
+
+            //initialize each with the first entry
+            Queue<List<int>>[] Walls = new Queue<List<int>>[4];
+            foreach (var wallSide in orderedWalls)
+            {
+                var q = new Queue<List<int>>();
+                q.Enqueue(new List<int> { 1 });
+                Walls[Convert.ToInt32(wallSide)] = q;
+            }
+
+
+
+            int wallSize = 1;
+
+            while (true)
+            {
+                int count = 1;
+                int wallPosition = 1;
+                int lastValue = 1;
+
+                foreach (var wallSide in orderedWalls)
+                {
+
+                    List<int> wallList = new List<int>(wallSize);
+
+
+                    //loop used to add things up
+                    for (int wallWalk = 0; wallWalk < wallSize; wallWalk++)
+                    {
+                        int total = lastValue;//always add in the last value
+
+                        //what do we add?
+                        if (wallPosition == wallSize)//we are at the corner
+                        {
+                            if(wallPosition > 1)
+                            {
+                                //do something except in first 2 cases?
+
+                            }    
+
+                        } else
+                        {
+                            //if we are at the corner - position 0
+                            if(wallPosition == 0)
+                            {
+
+                            } else
+                            {//else we take as many as we can - max of 3 off the wall and one trailer
+
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Wall: {0} Size: {1}", wallSide, wallSize);
+                    if(count > 1 && Convert.ToInt32(wallSide) % 2 == 1) {
+                        wallSize++;
+                    }
+
+                    wallPosition++;
+                    count++;
+                }
+
+                Console.ReadKey();
+            }
 
 
         }
